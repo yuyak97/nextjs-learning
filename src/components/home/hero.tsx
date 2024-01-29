7c2b7ff9202bc35f3cfbed46abd880a010b0bde1
Yuya Kayama <yuya.kayama97@gmail.com>
2024-01-29 20:04:59 +0100

    fix: hero styles

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,12 +5,7 @@ import React from "react"
 const Hero = () => {
   return (
     <div className="relative h-[400px]">
-      <Image
-        alt="hero"
-        src="/images/DALL·E_arcade_game.jpeg"
-        layout="fill"
-        objectFit="cover"
-      />
+      <Image alt="hero" src="/images/DALL·E_arcade_game.jpeg" fill priority />
       <div className="absolute inset-0 flex items-center justify-center bg-gray-400 bg-opacity-70 text-center dark:bg-gray-900 dark:bg-opacity-70">
         <h1
           className={`${pressStartFont.className} text-4xl font-bold text-black dark:text-white`}
